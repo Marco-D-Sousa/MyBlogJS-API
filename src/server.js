298214d0734e2d589738628473d9da2715920338
Marco Sousa <marcopaiva2004@gmail.com>
2023-10-20 16:06:59 +0200

    ajutando a port para .env

--- src/server.js
+++ src/server.js
@@ -7,6 +7,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
 	console.log(`Listening on http://localhost:${PORT} ...`);
 });
